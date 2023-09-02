e17c36bd4274f306f472fc637f76514561ddf96d
mbithi002 <142636252+mbithi002@users.noreply.github.com>
2023-09-02 14:42:20 -0700

    Add files via upload

---
+++ a_simple_calculator_project/simple_calculator/calc.js
@@ -0,0 +1,17 @@
+
+let displayValue = '';
+     
+function appendToDisplay(value) {
+  displayValue += value;
+  document.getElementById('display').value = displayValue;
+}
+
+function calculate() {
+  try {
+    displayValue = eval(displayValue).toString();
+    document.getElementById('display').value = displayValue;
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+  }
+}
+
